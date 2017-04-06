311f2aeedf7c58d9b92ca941a7e124466eaf3b23
tony <zhw65@pitt.edu>
2017-04-06 13:58:23 -0400

    add default value for map in connect

--- src/connect.js
+++ src/connect.js
@@ -3,10 +3,13 @@
  */
 'use strict'
 import {connect as reactReduxConnect} from 'react-redux'
-import {createRalativePathSelector,parsePaths} from 'dongbao-state'
+import {parsePaths} from 'dongbao-state'
 import {createCalcWithStates} from './calcWithStates'
 
 
+const mapToFirstState = (state)=>{
+  return state
+}
 
 // let options = {
 //   path:__dirname,
@@ -21,7 +24,7 @@ import {createCalcWithStates} from './calcWithStates'
 export function connect({
   path, // points to root if not provide
   states=["."], // points to current state if not provide
-  map // required
+  map = mapToFirstState // just map to the first state if not provide
 }) {
   let paths = parsePaths(path)
   return reactReduxConnect(createCalcWithStates(paths,states,map))
