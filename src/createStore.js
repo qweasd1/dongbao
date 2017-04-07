2b0d927689cb3c59d4ec4c1cbef7963b2ca7695e
tony <zhw65@pitt.edu>
2017-04-06 21:53:50 -0400

    use RootReducer from dongbao-state

--- src/createStore.js
+++ src/createStore.js
@@ -1,13 +1,12 @@
 /**
  * Created by tony on 4/4/17.
  */
-import {CreateRootReducer, initState as defaultInitState, Config} from "dongbao-state";
+import {RootReducer, initState as defaultInitState, Config} from "dongbao-state";
 import {createStore as reduxCreateStore} from "redux";
 
 
 export function createStore({enhancer, initState} = {}) {
-  let finalInitState = initState || defaultInitState
-  let store = reduxCreateStore(CreateRootReducer(), finalInitState, enhancer)
+  let store = reduxCreateStore(RootReducer, initState, enhancer)
   Config(store)
   return store
 }
