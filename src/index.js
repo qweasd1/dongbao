810d731722441de0ac400858b31912255f2a9458
tony <zhw65@pitt.edu>
2017-04-13 12:50:19 -0400

    export createStore, since other framework need this to do validation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import {render} from './render'
 import {connect} from './connect'
 import {State,Actions,States} from 'dongbao-state'
 import {createCalcWithStates} from './calcWithStates'
+import {createStore} from './createStore'
+
 
 
 module.exports = {
@@ -17,6 +19,8 @@ module.exports = {
   State,
   Actions,
   States,
+  
+  createStore,
   
   // utils
   createCalcWithStates
